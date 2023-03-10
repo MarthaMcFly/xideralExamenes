2161149aa162338d10374dba58350891f4befa7e
Marthx <marthaps007@gmail.com>
2023-03-10 00:10:27 -0600

    proyecto springboot con jdbc se copiaron las clases entity,service y dao

---
+++ Ex3-springMvcHibernateJdbcSBoot/src/main/java/com/luv2code/springdemo/entity/Alumno.java
@@ -0,0 +1,44 @@
+package com.luv2code.springdemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="customer")
+public class Alumno {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="nombre") //nombre de la columna
+	private String nombre;  //nombre con el que se guarda en Spring
+	
+	@Column(name="apellido")
+	private String apellido;
+	
+	@Column(name="grupo")
+	private String grupo;
+	
+	@Column(name="calificacion") //columna añadida
+	private float calificacion;
+	
+		
+}
+
+
+
+
+
