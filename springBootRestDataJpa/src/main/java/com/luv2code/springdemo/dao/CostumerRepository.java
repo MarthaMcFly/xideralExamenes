c820ea6703c49d40dc711acd5cb7c48497984660
Marthx <marthaps007@gmail.com>
2023-03-13 12:41:16 -0600

    proyecto SpringBoot Data Jpa

---
+++ springBootRestDataJpa/src/main/java/com/luv2code/springdemo/dao/CostumerRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springdemo.entity.Customer;
+
+public interface CostumerRepository extends JpaRepository<Customer,Integer>{
+
+}
---
+++ springBootRestDataJpa/src/main/java/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,65 @@
+package com.luv2code.springdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.dao.CostumerRepository;
+import com.luv2code.springdemo.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+private CostumerRepository customerRepository;
+	
+	@Autowired
+	public CustomerServiceImpl(CostumerRepository customerRespository) {
+		this.customerRepository = customerRespository;
+	}
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerRepository.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Customer saveCustomer(Customer theCustomer) {
+		Customer customer = customerRepository.save(theCustomer);
+		return customer;
+	}
+
+	@Override
+	@Transactional
+	public Customer getCustomer(int theId) {
+		
+		Optional<Customer> result = customerRepository.findById(theId);
+
+        Customer customer = null;
+
+        if (result.isPresent()) {
+        	customer = result.get();
+        }
+        else {
+            // we didn't find the customer
+            throw new RuntimeException("Did not find customer id - " + theId);
+        }
+
+        return customer;
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(int theId) {
+		
+		customerRepository.deleteById(theId);
+	}
+}
+
+
+
+
+
